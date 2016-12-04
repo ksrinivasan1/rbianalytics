8d544e0daf53e8a8c7e14596cb89439a1bbb3c91
Kartik Srinivasan <kartiksr@in.ibm.com>
2016-12-04 21:37:10 +0530

    Added downloads2 directory with the files downloaded

--- bin/downloadData.sh
+++ bin/downloadData.sh
@@ -4,11 +4,14 @@ declare -a years=(2009 2010 2011 2012 2013 2014 2015 2016)
 
 for y in `echo ${years[@]}`
 do
-  echo ${y}
+   echo ${y}
+   mkdir -p ${cwd}/downloads2/${y}
+   cd ${cwd}/downloads2/${y}
    for f in `cat ${cwd}/extract/${y}.html | grep -io "[0-9A-Za-z]*.XLS" | grep -v "Images"`
       do
-   	wget https://rbidocs.rbi.org.in/rdocs/NEFT/DOCs/$f -o ${cwd}/downloads/$y/$f
+   	wget https://rbidocs.rbi.org.in/rdocs/NEFT/DOCs/$f
         echo $y/$f
       done
+   cd ${cwd}
 done
 
