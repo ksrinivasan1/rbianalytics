f764b789d68261ed39298ac9c28b78430b3191ee
Kartik Srinivasan <kartiksr@in.ibm.com>
2016-12-08 00:47:13 +0530

    Added avgTxn to dataset. Added graphs for breakup by year, month and by bank type.

--- src/Neft_Kartik_Analysis.R
+++ src/Neft_Kartik_Analysis.R
@@ -4,27 +4,38 @@ library(ggplot2)
 #source(file = paste(workingDirectory, "/RbiNeft.R") )
 
 # TxnValue By year
-tapply(neftData$TotalTxnVal, neftData$Year, summary)
-tapply(neftData$TotalTxnVal, neftData$Year, mean)
-tapply(neftData$TotalTxnVal, neftData$Year, sd)
+tapply(neftDataMerged$TotalTxnVal, neftData$Year, summary)
+tapply(neftDataMerged$TotalTxnVal, neftData$Year, mean)
+tapply(neftDataMerged$TotalTxnVal, neftData$Year, sd)
 # Txns By year
-tapply(neftData$TotalTxns, neftData$Year, summary)
-tapply(neftData$TotalTxns, neftData$Year, mean)
-tapply(neftData$TotalTxns, neftData$Year, sd)
+tapply(neftDataMerged$TotalTxns, neftData$Year, summary)
+tapply(neftDataMerged$TotalTxns, neftData$Year, mean)
+tapply(neftDataMerged$TotalTxns, neftData$Year, sd)
 
 # By month
-tapply(neftData$TotalTxnVal, neftData$Month, summary)
-tapply(neftData$TotalTxnVal, neftData$Month, mean)
-tapply(neftData$TotalTxnVal, neftData$Month, sd)
+tapply(neftDataMerged$TotalTxnVal, neftData$Month, summary)
+tapply(neftDataMerged$TotalTxnVal, neftData$Month, mean)
+tapply(neftDataMerged$TotalTxnVal, neftData$Month, sd)
 
-
-myPlot <- ggplot2::ggplot(data=neftData, aes(x=Month, y=TotalTxns))
+# By month
+myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxns))
 myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Month) + theme(axis.text.x=element_blank())
 
-
-myPlot <- ggplot2::ggplot(data=neftData, aes(x=Month, y=TotalTxnVal))
+myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxnVal))
 myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Month) + theme(axis.text.x=element_blank())
 
+myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=AvgTxnVal))
+myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Month) + theme(axis.text.x=element_blank())
 
-#myPlot <- ggplot2::ggplot(data=neftData, aes(x=MonthAndYear, y=TotalTxns))
-#myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Bank) + theme(axis.text.x=element_blank())
+# By month and broken up by bank type to see how number of transactions have played over months
+ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxns, fill=Type)) + geom_bar(stat="sum")
+#ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxnVal, fill=Type)) + geom_bar(stat="sum")
+#ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=AvgTxnVal, fill=Type)) + geom_bar(stat="sum")
+
+# By year and broken up by bank type to see how number of transactions have increased over years
+ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxns, fill=Type)) + geom_bar(stat="sum")
+#ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxnVal, fill=Type)) + geom_bar(stat="sum")
+#ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=AvgTxnVal, fill=Type)) + geom_bar(stat="sum")
+
+# By month and broken up by bank type to see how number of transactions have played over months
+ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxns, fill=Filtered)) + geom_bar(stat="sum")
--- src/RbiNeft.R
+++ src/RbiNeft.R
@@ -5,6 +5,8 @@
 #dataDirectory    <- paste(workingDirectory, "/data/")
 #setwd(dirname(dataDirectory))
 
+rm(list = ls())     # clear objects 
+
 setwd("/Users/ksrinivasan/Desktop/GMITE/Assignments/Statistics/final/rbianalytics/")
 
 # load up the bank classifications from working directory
@@ -15,23 +17,25 @@ bankClsfn <- readxl::read_excel(path = "data/Classification.xlsx", sheet = "To_c
 neftData  <- c()
 fileNames <- c( "data/yearwise/2009.xlsx", "data/yearwise/2010.xlsx", "data/yearwise/2011.xlsx", "data/yearwise/2012.xlsx")
 for(i in 1:length(fileNames)) {
-  #Specifically load only the NEFT Sheets
-  annualData  <- readxl::read_excel(path =fileNames[i], sheet = "NEFT")
+  annualData  <- readxl::read_excel(path =fileNames[i], sheet = "NEFT")   #Specifically load only the NEFT Sheets
   neftData    <- rbind(neftData, annualData)
 }
 
-# enhance the data set with some custom fields.
+# enhance the data set with some custom fields (monthAndYear, Totaltxns, totalvalue and avgvalue).
 monthAndYear  <- c()
 totalTxns     <- c()
 totalTxnValue <- c()
+avgTxnValue   <- c()
 for(i in 1:length(neftData$OutwardTransactions)) {
   monthAndYear [ i ]  <- paste(neftData$Month [ i ] , neftData$Year [ i ], sep = " ")
-  totalTxns    [ i ]  <- neftData$OutwardTransactions[ i ]  + neftData$InwardTransactions[ i ]
-  totalTxnValue[ i ]  <- neftData$OutwardValueMillions[ i ] + neftData$InwardValueMillions[ i ]
+  totalTxns    [ i ]  <- neftData$OutwardTransactions  [ i ] + neftData$InwardTransactions [ i ]
+  totalTxnValue[ i ]  <- neftData$OutwardValueMillions [ i ] + neftData$InwardValueMillions[ i ]
+  avgTxnValue  [ i ]  <- totalTxnValue[ i ] / totalTxns[ i ]
 }
 neftData$MonthAndYear <- monthAndYear
 neftData$TotalTxns    <- totalTxns
 neftData$TotalTxnVal  <- totalTxnValue
+neftData$AvgTxnVal    <- avgTxnValue
 
 # join the bank data with classifications so we can get clean bank name and type
 neftDataMergedUnclean <- merge(neftData, bankClsfn, by="Bank")
