dcbce88761853a0de1bcf60bf86f85e8d3756cb5
yashlondhe <yashlondhe@users.noreply.github.com>
2016-12-08 19:10:46 +0530

    Update the script with changes from type to Sector, and sort the graph

--- src/Neft_Kartik_Analysis.R
+++ src/Neft_Kartik_Analysis.R
@@ -4,24 +4,24 @@ library(ggplot2)
 #source(file = paste(workingDirectory, "/RbiNeft.R") )
 
 # Txn Info By year
-tapply(neftDataMerged$TotalTxnVal, neftData$Year, summary)
-tapply(neftDataMerged$TotalTxnVal, neftData$Year, mean)
-tapply(neftDataMerged$TotalTxnVal, neftData$Year, sd)
-tapply(neftDataMerged$TotalTxns, neftData$Year, summary)
-tapply(neftDataMerged$TotalTxns, neftData$Year, mean)
-tapply(neftDataMerged$TotalTxns, neftData$Year, sd)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Year, summary)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Year, mean)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Year, sd)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Year, summary)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Year, mean)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Year, sd)
 
 # Txn Info By month
-tapply(neftDataMerged$TotalTxnVal, neftData$Month, summary)
-tapply(neftDataMerged$TotalTxnVal, neftData$Month, mean)
-tapply(neftDataMerged$TotalTxnVal, neftData$Month, sd)
-tapply(neftDataMerged$TotalTxns, neftData$Month, summary)
-tapply(neftDataMerged$TotalTxns, neftData$Month, mean)
-tapply(neftDataMerged$TotalTxns, neftData$Month, sd)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Month, summary)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Month, mean)
+tapply(neftDataMerged$TotalTxnVal, neftDataMerged$Month, sd)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Month, summary)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Month, mean)
+tapply(neftDataMerged$TotalTxns, neftDataMerged$Month, sd)
 
 # By month
-#myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxns))
-#myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Month) + theme(axis.text.x=element_blank())
+myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=factor(Month, levels = month.abb), y=TotalTxns))
+myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~factor(Month, levels = month.abb)) + theme(axis.text.x=element_blank())
 
 #myPlot <- ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=TotalTxnVal))
 #myPlot + geom_point(shape=1,alpha=1/10) + facet_wrap(~Month) + theme(axis.text.x=element_blank())
@@ -35,13 +35,13 @@ tapply(neftDataMerged$TotalTxns, neftData$Month, sd)
 #ggplot2::ggplot(data=neftDataMerged, aes(x=Month, y=AvgTxnVal, fill=Type)) + geom_bar(stat="sum")
 
 #Over months
-ggplot2::ggplot(data=neftDataMerged, aes(x=factor(Month, levels = monthNames), y=TotalTxns, fill=Type)) + geom_bar(stat="sum")
+ggplot2::ggplot(data=neftDataMerged, aes(x=factor(Month, levels = month.abb), y=TotalTxns, fill=Sector)) + geom_bar(stat="sum")
 
 #Over months and over the years
-ggplot2::ggplot(data=neftDataMerged, aes(x=factor(Month, levels = monthNames), y=TotalTxns, fill=Type)) + geom_bar(stat="sum") + facet_wrap(~Year) + theme(axis.text.x=element_blank()) 
+ggplot2::ggplot(data=neftDataMerged, aes(x=factor(Month, levels = month.abb), y=TotalTxns, fill=Sector)) + geom_bar(stat="sum") + facet_wrap(~Year) + theme(axis.text.x=element_blank()) 
 
 # By year and broken up by bank type to see how number of transactions have increased over years
-ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxns, fill=Type)) + geom_bar(stat="sum")
+ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxns, fill=Sector)) + geom_bar(stat="sum")
 #ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=TotalTxnVal, fill=Type)) + geom_bar(stat="sum")
 #ggplot2::ggplot(data=neftDataMerged, aes(x=Year, y=AvgTxnVal, fill=Type)) + geom_bar(stat="sum")
 
